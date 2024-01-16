3f7aa259833859b2642053cf76c7de3863f5caad
Hemlo <ravi864654@gmail.com>
2024-01-17 02:24:12 +0530

    Add loader for products

--- src/PizzaContext.js
+++ src/PizzaContext.js
@@ -11,33 +11,32 @@ export const usePizzaContext = () => useContext(PizzaContext);
 
 const PizzaProvider = ({ children }) => {
   const [menu, setMenu] = useState('pizza');
-  const [user, setUser] = useState({username:'',fullName:''});
+  const [user, setUser] = useState({ username: '', fullName: '' });
   const [cart, setCart] = useState([]);
   const [totalProductPrice, setTotalProductPrice] = useState('');
   const [product, setProduct] = useState([]);
   const [isCheckoutDisplay, setIsCheckoutDisplay] = useState(false);
   const { showBoundary } = useErrorBoundary();
+  const [isProductLoading, setIsProductLoading] = useState(true);
 
   const fetchProduct = async () => {
-    try{
-
-  
-    const fetchAllProduct = await fetch(
-      `${process.env.REACT_APP_BACKEND_URL}/api/allProducts`
-    );
-    const fetchProductJson = await fetchAllProduct.json();
-    setProduct(fetchProductJson.allProducts);
-    }catch(err){
-      toast.error('Failed to fetch product')
+    try {
+      const fetchAllProduct = await fetch(
+        `${process.env.REACT_APP_BACKEND_URL}/api/allProducts`
+      );
+      const fetchProductJson = await fetchAllProduct.json();
+      setProduct(fetchProductJson.allProducts);
+      setIsProductLoading(false);
+    } catch (err) {
+      toast.error('Failed to fetch product');
       showBoundary(err);
     }
   };
 
   useEffect(() => {
-
-      fetchProduct();
-      // Fetching cart
-      fetchProductCart(showBoundary).then((data) => setCart(data));
+    fetchProduct();
+    // Fetching cart
+    fetchProductCart(showBoundary).then((data) => setCart(data));
 
     // Accessing previous logged in user
     if (localStorage.getItem('foody_token') !== null) {
@@ -73,6 +72,7 @@ const PizzaProvider = ({ children }) => {
         totalProductPrice,
         isCheckoutDisplay,
         setIsCheckoutDisplay,
+        isProductLoading,
       }}
     >
       {children}
--- src/components/ProductContainer.js
+++ src/components/ProductContainer.js
@@ -2,11 +2,21 @@ import React from 'react';
 import '../scss/product_container.scss';
 import { usePizzaContext } from '../PizzaContext';
 import ProductBox from './ProductBox';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 const ProductContainer = () => {
-  const { menu,product } = usePizzaContext();
+  const { menu,product,isProductLoading } = usePizzaContext();
   
   return (
       <div className="product-container">
+        {isProductLoading && 
+        
+        
+        Array(5).fill(0).map((_,i)=>(<div key={i} className="product">
+        <Skeleton className='product-image'/>
+        <Skeleton count={2} className='skeleton'/>       
+          </div>))   }
+      {(!isProductLoading && product.length === 0) && <p id="no_order_diplay">Oops... Looks like there is no product</p>}
         {product
           ?.filter((ele) => ele.category === menu)
           .map((ele) => <ProductBox value={ele} key={ele._id}/>)}
